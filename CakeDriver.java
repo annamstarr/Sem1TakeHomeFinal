5526094d1367268258d111bc9f93c1a173d132aa
jhstarr <jeffhstarr@gmail.com>
2018-01-07 10:24:58 -0800

    changed name

--- CakeDriver.java
+++ CakeDriver.java
@@ -4,7 +4,7 @@
  * January 2018 - Semester 1 Take Home Final
  * Part 2 - Driver
  */
-public class Baker {
+public class CakeDriver {
     // 1. Main method that initiates two objects
     public static void main(String[] args) {
         Cake c1 = new Cake("Chocolate", 3);
